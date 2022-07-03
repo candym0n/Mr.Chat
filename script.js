e46a733aa0006ffdd2e3483545ade1674894a829
candym0n <93819596+candym0n@users.noreply.github.com>
2022-07-03 18:48:21 +0800

    Made it so that you can speak

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ document.body.style.width = window.innerWidth + 'px';
 }
 resize()
 window.addEventListener('resize',resize)
+var ui = document.querySelector('#userInput')
+var uiHolder = document.querySelector('#user')
+document.querySelector('body').removeAttribute("formaction");
+ui.addEventListener('keydown',function(e){
+	var key = e.key.toLowerCase();
+  if(key == 'enter'){
+  	submitToMrChat(ui.value)
+  }
+})
+function submitToMrChat(thing){
+	const chat = document.querySelector("#chats").appendChild(document.createElement('div'))
+  chat.id = 'chatBoxForUser'
+  chat.innerHTML = thing;
+  ui.value = '';
+  processStuff(thing)
+ }
+ function processStuff(thing){
+ 	//This is where Mr.Chat comes in :)
+ }
